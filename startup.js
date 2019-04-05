c0ae4be4cb9f74c77490974ff1786e6e672479c2
ilius33 <ilius33@mail.ru>
2019-04-05 16:01:02 +0400

    new map format with map and mask sections

--- startup.js
+++ startup.js
@@ -4,36 +4,52 @@ var customMapFormat = {
 
     toString: function(map, fileName) {
 		var layer = map.layerAt(0);
-		var horizontalBorder = 'D:' + new Array(layer.width + 3).join('#');
 		var asciiMap = '';
-		var dictionary = { '-1': ' ' };
+		var asciiMask = '';
+
+		var horizontalBorder = function (prefix) { return prefix + ':' + new Array(layer.width + 3).join('#'); };
+		var mapDictionary = { '-1': ' ' };
+		var maskDictionary = { '-1': ' ' };
 
 		if (layer.isTileLayer) {
-			asciiMap = horizontalBorder;
+			asciiMap = horizontalBorder('map');
+			asciiMask = horizontalBorder('mask');
+
 			for (y = 0; y < layer.height; ++y) {
-				var row = '';
+				var mapRow = '';
+				var maskRow = '';
 				for (x = 0; x < layer.width; ++x) {
-					var tile, character, tileId;
+					var tile, mapChar, maskChar, tileId;
 					tileId = layer.cellAt(x, y).tileId;
 
-					if (!!dictionary[tileId]) {
-						character = dictionary[tileId];
+					if (!!mapDictionary[tileId]) {
+						mapChar = mapDictionary[tileId];
+						maskChar = maskDictionary[tileId];
 					} else {
 						tile = tileId != -1 ? layer.tileAt(x, y) : null;
-						character = tile ? tile.type || tile.property('name') || ' ' : ' ';
-						dictionary[tileId] = character;
+						mapChar = tile ? tile.property('map') || ' ' : ' ';
+						maskChar = tile ? tile.property('mask') || tile.property('map') || ' ' : ' ';
+						mapDictionary[tileId] = mapChar;
+						maskDictionary[tileId] = maskChar;
 					}
 
-					row += character;
+					mapRow += mapChar;
+					maskRow += maskChar;
 				}
-				asciiMap += '\nD:#' + row + '#';
+				asciiMap += '\nmap:#' + mapRow + '#';
+				asciiMask += '\nmask:#' + maskRow + '#';
 			}
-			asciiMap += '\n' + horizontalBorder;
+			asciiMap += '\n' + horizontalBorder('map');
+			asciiMask += '\n' + horizontalBorder('mask');
 		}
 
+		asciiMap += '\n\n';
+		asciiMap += asciiMask;
+
 		tiled.log('Done!');
+
         return asciiMap;
     },
-}
+};
 
 tiled.registerMapFormat('Andgband.online', customMapFormat);
